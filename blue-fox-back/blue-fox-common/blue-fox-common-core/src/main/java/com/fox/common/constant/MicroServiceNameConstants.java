686b8cc0d797929f1f00fbf79f5a89d77d39e290
刘鹏飞 <35693850+lpf-exclusive@users.noreply.github.com>
2021-11-26 17:52:01 +0800

    Add files via upload

---
+++ blue-fox-back/blue-fox-common/blue-fox-common-core/src/main/java/com/fox/common/constant/MicroServiceNameConstants.java
@@ -0,0 +1,20 @@
+package com.fox.common.constant;
+
+/**
+ * 微服务名称常量
+ * <p>
+ * Feign 调用声明使用
+ *
+ * @author <a href="mailto:xianrui0365@163.com">haoxr</a>
+ * @date 2021/10/30 9:32
+ */
+public interface MicroServiceNameConstants {
+
+    String ADMIN = "blue-fox-admin";
+    String AUTH = "blue-fox-auth";
+    String GATEWAY = "blue-fox-gateway";
+    String MALL_UMS = "blue-fox_ums";
+    String MALL_PMS = "blue-fox_pms";
+    String MALL_OMS = "blue-fox_oms";
+    String MALL_SMS = "blue-fox_sms";
+}
---
+++ blue-fox-back/blue-fox-common/blue-fox-common-core/src/main/java/com/fox/common/constraint/CityType.java
@@ -0,0 +1,9 @@
+package com.fox.common.constraint;
+
+/**
+ * @author Gadfly
+ * @since 2021-08-06 16:02
+ */
+public enum CityType {
+    PROVINCE,CITY,AREA
+}
