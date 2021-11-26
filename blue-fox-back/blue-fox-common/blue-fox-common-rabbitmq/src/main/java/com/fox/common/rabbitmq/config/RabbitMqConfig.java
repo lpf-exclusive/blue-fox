63ebf6ea551f2a26f83fbd6b1dfcff5606808d50
刘鹏飞 <35693850+lpf-exclusive@users.noreply.github.com>
2021-11-26 17:53:05 +0800

    Add files via upload

---
+++ blue-fox-back/blue-fox-common/blue-fox-common-rabbitmq/src/main/java/com/fox/common/rabbitmq/config/RabbitMqConfig.java
@@ -0,0 +1,16 @@
+package com.fox.common.rabbitmq.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @desc
+ * @email huawei_code@163.com
+ * @date 2021/1/17
+ */
+@Configuration
+@EnableTransactionManagement
+public class RabbitMqConfig {
+
+
+}
