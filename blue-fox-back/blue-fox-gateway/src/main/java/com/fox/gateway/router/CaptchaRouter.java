23287b1db116e35e29cdf9ba99247f2cb15f34c3
刘鹏飞 <35693850+lpf-exclusive@users.noreply.github.com>
2021-11-26 17:49:45 +0800

    Add files via upload

---
+++ blue-fox-back/blue-fox-gateway/src/main/java/com/fox/gateway/router/CaptchaRouter.java
@@ -0,0 +1,28 @@
+package com.fox.gateway.router;
+
+import com.fox.gateway.kaptcha.handler.CaptchaHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+/**
+ * 图片验证码路由
+ *
+ * @author <a href="mailto:xianrui0365@163.com">haoxianrui</a>
+ * @date 2021/10/4
+ */
+@Configuration
+public class CaptchaRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> routeFunction(CaptchaHandler captchaHandler) {
+        return RouterFunctions
+                .route(RequestPredicates.GET("/captcha")
+                        .and(RequestPredicates.accept(MediaType.TEXT_PLAIN)), captchaHandler::handle);
+    }
+
+}
