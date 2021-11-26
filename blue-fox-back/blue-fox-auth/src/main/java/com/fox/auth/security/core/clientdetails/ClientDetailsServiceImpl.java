ac20fa28d285bc326d820306fdb613093d6746b0
刘鹏飞 <35693850+lpf-exclusive@users.noreply.github.com>
2021-11-26 17:42:28 +0800

    Add files via upload

---
+++ blue-fox-back/blue-fox-auth/src/main/java/com/fox/auth/security/core/clientdetails/ClientDetailsServiceImpl.java
@@ -0,0 +1,51 @@
+package com.fox.auth.security.core.clientdetails;
+
+import com.fox.admin.api.OAuthClientFeignClient;
+import com.fox.admin.pojo.entity.SysOauthClient;
+import com.fox.auth.common.enums.PasswordEncoderTypeEnum;
+import com.fox.common.result.Result;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.NoSuchClientException;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
+import org.springframework.stereotype.Service;
+
+/**
+ * OAuth2 客户端信息
+ */
+@Service
+@RequiredArgsConstructor
+public class ClientDetailsServiceImpl implements ClientDetailsService {
+
+    private final OAuthClientFeignClient oAuthClientFeignClient;
+
+    @Override
+    @Cacheable(cacheNames = "auth", key = "'oauth-client:'+#clientId")
+    public ClientDetails loadClientByClientId(String clientId) {
+        try {
+            Result<SysOauthClient> result = oAuthClientFeignClient.getOAuthClientById(clientId);
+            if (Result.success().getCode().equals(result.getCode())) {
+                SysOauthClient client = result.getData();
+                BaseClientDetails clientDetails = new BaseClientDetails(
+                        client.getClientId(),
+                        client.getResourceIds(),
+                        client.getScope(),
+                        client.getAuthorizedGrantTypes(),
+                        client.getAuthorities(),
+                        client.getWebServerRedirectUri()
+                );
+                clientDetails.setClientSecret(PasswordEncoderTypeEnum.NOOP.getPrefix() + client.getClientSecret());
+                clientDetails.setAccessTokenValiditySeconds(client.getAccessTokenValidity());
+                clientDetails.setRefreshTokenValiditySeconds(client.getRefreshTokenValidity());
+                return clientDetails;
+            } else {
+                throw new NoSuchClientException("No client with requested id: " + clientId);
+            }
+        } catch (EmptyResultDataAccessException var4) {
+            throw new NoSuchClientException("No client with requested id: " + clientId);
+        }
+    }
+}
